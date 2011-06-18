cfc843127d2201b1a315cc8b208ad0b4aa9e83e4
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-06-18 13:10:26 +0000

    add some documentation

--- lib/Geo/Gosmore.pm
+++ lib/Geo/Gosmore.pm
@@ -6,9 +6,51 @@ use Geo::Gosmore::Route;
 use File::Basename qw(dirname);
 use Cwd qw(getcwd);
 
+=encoding utf8
+
 =head1 NAME
 
 Geo::Gosmore - Interface to the headless L<gosmore(1)>
+
+=head1 SYNOPSIS
+
+First install L<gosmore(1)>, e.g. on Debian:
+
+    sudo aptitude install gosmore
+
+Then build a F<gosmore.pak> file:
+
+    wget http://download.geofabrik.de/osm/europe/british_isles.osm.bz2
+    # pv(1) is not needed, it just shows you the import progress
+    bzcat british_isles.osm.bz2 | pv | gosmore rebuild
+
+Then use this library, with C<$gosmore_pak> being the full path to
+your new F<gosmore.pak>.
+
+    my $gosmore = Geo::Gosmore->new(
+        gosmore_pak => $gosmore_pak,
+    );
+
+    my $query = Geo::Gosmore::Query->new(
+        flat => '51.5425',
+        flon => '-0.111',
+        tlat => '51.5614',
+        tlon => '-0.0466',
+        fast => 1,
+        v    => 'motorcar',
+    );
+    my $route = $gosmore->find_route($query);
+    my $distance = $route->distance;
+
+=head1 DESCRIPTION
+
+Provides an interface to the headless version of the
+L<gosmore|http://wiki.openstreetmap.org/wiki/Gosmore> routing
+library. When compiled with headless support it provides a simple
+interface to do routing. This library just parses its simple output
+and provides accessors for it.
+
+This is experimental software with an API subject to change.
 
 =cut
 
@@ -62,6 +104,16 @@ sub find_route {
 
     return $route;
 }
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Ævar Arnfjörð Bjarmason <avar@cpan.org>
+
+This program is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
 1;
 
