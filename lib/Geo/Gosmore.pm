8472a221081e26061675d4e77ca504a65729b0fa
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-06-18 19:38:45 +0000

    it's actually remaining time.

--- lib/Geo/Gosmore.pm
+++ lib/Geo/Gosmore.pm
@@ -105,8 +105,8 @@ sub route {
         # We're getting a stream of lat/lon values
         next unless $line =~ /^[0-9]/;
 
-        my ($lat, $lon, $junction_type, $style, $heap_idx, $name) = split /,/, $line;
-        push @points => [ $lat, $lon, $junction_type, $style, $heap_idx, $name ];
+        my ($lat, $lon, $junction_type, $style, $remaining_time, $name) = split /,/, $line;
+        push @points => [ $lat, $lon, $junction_type, $style, $remaining_time, $name ];
     }
 
     my $route = Geo::Gosmore::Route->new(
